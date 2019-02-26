a88c86bbeabb7b952563dfb4565e732540ee7881
bellice <bellice@tuta.io>
2019-02-27 00:48:04 +0100

    add browser-sync

--- gulpfile.js
+++ gulpfile.js
@@ -2,20 +2,16 @@ const  gulp = require("gulp");
 const browserSync = require("browser-sync").create();
 
  
+//Start BrowserSync
 gulp.task("sync", function() {
 	browserSync.init({
 		proxy: "http://127.0.0.1:8080/mytikas/"
 	});
+	//Reload app whenever HTML, JS or CSS files change
+	gulp.watch("*.html").on("change", browserSync.reload); 
+	gulp.watch("src/*.js").on("change", browserSync.reload);
+	gulp.watch("src/*.css").on("change", browserSync.reload); 
 });
-
-
-
-
-
-
-
-
-
 
 
 
